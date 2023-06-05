6d3bed3103b150470b7c071b272e1a328ad60f60
BabitaSingh29 <babitapatwal2000@gmail.com>
2023-06-05 17:16:37 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
+
 def get_requirements(file_path:str)->List[str]:
     requirement=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirement:
+            requirements.remove(HYPEN_E_DOT)
 
         return requirements
 
